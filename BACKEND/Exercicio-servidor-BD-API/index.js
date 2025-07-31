b3678fb33e19c3176ed83fa94f8e6736ebdc8025
Evandro dos Santos Dias <vando86206@gmail.com>
2025-07-31 14:45:27 -0300

    inserção e mostrar tabela

--- BACKEND/Exercicio-servidor-BD-API/index.js
+++ BACKEND/Exercicio-servidor-BD-API/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const PORT = 3001
 app.use(bodyParser.json())
+app.use(cors())
 const db = require('./databases/dbCadastro')
 
 app.get('/clientes', (req, res) => {
