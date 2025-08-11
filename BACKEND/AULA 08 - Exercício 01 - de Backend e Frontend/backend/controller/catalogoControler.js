657e4dc539f9bd61de1356343eb73a6f324e7f6f
Evandro dos Santos Dias <vando86206@gmail.com>
2025-08-11 16:02:12 -0300

    add arquivo routes

--- BACKEND/AULA 08 - Exercício 01 - de Backend e Frontend/backend/controller/catalogoControler.js
+++ BACKEND/AULA 08 - Exercício 01 - de Backend e Frontend/backend/controller/catalogoControler.js
@@ -23,22 +23,41 @@ export const getPlayForId = async (req, res) => {
 export const addPlay = async (req, res) => {
     const { nome, tipo, ano } = req.body
     try {
-        const [rows] = await pool.execute(
+        const [result] = await pool.execute(
             `insert into catalogo (nome, tipo, ano) VALUES (?,?,?)`, [nome, tipo, ano])
         res.status(201).json({ mensagem: "Jogo inserido com suceso" })
     } catch (error) {
         res.status(500).json({ erro: 'Erro ao inserir jogo!!' })
     }
 }
+
 export const updatePlay = async (req, res) => {
     const { id } = req.params
     const { nome, tipo, ano } = req.body
     try {
-        const [rows] = await pool.execute(
+        const [result] = await pool.execute(
             `update catalogo set nome = ?, tipo = ?, ano = ? where id = ?`, [nome, tipo, ano, id])
         res.status(201).json({ mensagem: "Jogo atualizado com suceso" })
     } catch (error) {
         res.status(500).json({ erro: 'Erro ao atualizar jogo!!' })
+    }
+}
+
+export const deletePlay = async (req, res) => {
+    const { id } = req.params
+    try {
+        const [result] = await pool.execute(
+            `DELELE FROM catalogo where id = ?`, [id]
+        )
+        /** "affectedRows" ==>> função ou propriedade que retorna o número de 
+         * linhas afetadas por uma operação de escrita em 
+         * um banco de dados, como INSERT, UPDATE, REPLACE ou DELETE. 
+         * Em outras palavras, ela indica quantas linhas foram 
+         * modificadas ou excluídas em uma consulta.  */
+        if (result.affectedRows === 0) return res.status(404).json({ mensagem: 'Jogo não encontrado!!' })
+        res.json({ mensagem: 'Jogo excluido com sucesso!!' })
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao deletar jogo' });
     }
 }
 
---
+++ BACKEND/AULA 08 - Exercício 01 - de Backend e Frontend/backend/routes/catalogoRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import {
+    getAll,
+    getPlayForId,
+    addPlay,
+    updatePlay,
+    deletePlay
+} from '../controller/catalogoControler.js'
+
+router = express.Router()
+
+
+
+
